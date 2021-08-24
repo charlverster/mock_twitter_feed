e80dfc97f3e56b029dc545a1268bee890b8f03eb
charlverster <33346370+charlverster@users.noreply.github.com>
2021-08-24 14:47:36 +0200

    Added method to get the path to the workding dir \n Added method to check working dir only contains two files

--- app/mock_twitter_feed.py
+++ app/mock_twitter_feed.py
@@ -1,12 +1,32 @@
 # Mock Twitter Feed
+import os
+import sys
 
-# mock function to check testing is working
-def check_for_files(x,y):
-    """Checks that working directory contains two text files named 'user.txt' and 'tweets.txt'"""
-    return 2
+def get_path_to_input_files():
+    """Gets path to folder where input files are expected."""
+    try:
+        path = os.getcwd() + "/app/files" # Relative path to working dir expected to contain input files
+    except OSError:
+        print("Could not find the current working directory. Quiting program.")
+        sys.exit()
+    else:
+        return path
 
-
-
+def check_for_only_two_files(file_path):
+    """Checks that working directory contains two files."""
+    files = os.listdir(file_path)       # Generates a list of files in working dir
+              
+    try: 
+        len(files)       # Counts files in list
+    except ValueError as e:
+        if int(e) != 2:
+            print(f"Expected 2 files, found {num_of_files}. Quiting program.")
+            sys.exit()
+        else:
+            print(int(e))
+g
 if __name__ == '__main__':
     # Run the app
-    pass
+    path = get_path_to_input_files()
+    check_for_only_two_files(path)
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
 from mock_twitter_feed import * #app installed as package, import so its accessible to tests
 
-#Assumption: Program is run in a directory containing the relevant files
-def test_check_for_files():
-    assert check_for_files() == 2 # Checks that directory only contains two text files
+# def test_get_path_to_input_files():
+#     assert get_path_to_input_files() == "/Users/charlverster/Documents/GitHub/mock_twitter_feed/app/files"
+    # Works. Don't need to keep testing this
 
+#Assumption: Program is run in a directory containing a folder 'files' with the relevant files
+def test_check_for_only_two_files():
+    path = "/Users/charlverster/Documents/GitHub/mock_twitter_feed/app/files"
+    check_for_only_two_files(path) # Checks that directory only contains two text files
