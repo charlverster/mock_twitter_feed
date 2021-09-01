6f301daa2b87b7190a8179db0268dab7ef584f7f
charlverster <33346370+charlverster@users.noreply.github.com>
2021-09-01 08:33:28 +0200

    Updated: test_read_text_files.py

--- src/app/test_read_text_files.py
+++ src/app/test_read_text_files.py
@@ -1,25 +1,27 @@
 import unittest
 import read_text_files as rd
+import os
 
 class TestGetMethods(unittest.TestCase):
         
     def test_get_path_to_input_files(self):
         # Arrange
-        path = 'usr/src/app/files'
+        expected_result = '/usr/src/app/files'
         # Act
-        result = rd.get_path_to_input_files()
+        test = rd.get_path_to_input_files()
         # Assert
-        self.assertEqual(str(path),str(result))
+        self.assertEqual(str(test),str(expected_result))
 
     def test_read_file(self):
         # Arrange
-        filepath = "usr/src/app/test_files/test_user.txt"
+        filepath =  "/usr/src/app/test_files/test_user.txt"
         split_on = " "
         drop_list = [" follows",","]
+        expected_result = [['Ward', 'Alan'], ['Alan', 'Martin'], ['Ward', 'Martin', 'Alan']]
         # Act
-        result = rd.read_file(filepath,split_on,drop_list)
+        test = rd.read_file(filepath,split_on,drop_list)
         # Assert
-        self.assertEqual(result,[['Ward', 'Alan'], ['Alan', 'Martin'], ['Ward', 'Martin', 'Alan']])
+        self.assertEqual(test,expected_result)
 
 if __name__ == '__main__':
     unittest.main()
